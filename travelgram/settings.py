d31beaa1ddf1563df6ecf4ac61d86b1eafa58bc6
jarolect <dawjarole@gmail.com>
2021-06-24 23:33:49 +0200

    commit con static dentro y settings perfectos

--- travelgram/settings.py
+++ travelgram/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tj@i_kns%0vt8=phyr8%_oi=_a3c#4@ff*@(i%$p_k6qibb=y5'
+with open(os.path.join(BASE_DIR,'secret_key.txt')) as f:
+    SECRET_KEY =f.read().strip() 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -124,12 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[BASE_DIR / 'static']
+STATICFILES_DIRS=[BASE_DIR/ 'travelgram/static']
 STATICFILES_FINDERS=[
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
-STATIC_ROOT=BASE_DIR/'travelgram/static'
+STATIC_ROOT=BASE_DIR/'static1'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
